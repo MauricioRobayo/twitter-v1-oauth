1954a9d51bc1e6f9cab28c52e01881c294b081a7
Mauricio Robayo <rfmajo@gmail.com>
2021-07-06 07:59:41 -0500

    chore: add helpers
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ src/authorization/helpers.ts
@@ -0,0 +1,23 @@
+import crypto = require("crypto");
+
+// https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent#Description
+export function percentEncode(str: string): string {
+  return encodeURIComponent(str)
+    .replace(/[!'()]/g, escape)
+    .replace(/\*/g, "%2A");
+}
+
+export function timestamp(): number {
+  return Math.round(Date.now() / 1000);
+}
+
+export function randomString(length: number): string {
+  if (length <= 0) {
+    return "";
+  }
+
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .substring(0, length);
+}
